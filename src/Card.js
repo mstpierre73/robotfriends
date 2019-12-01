037548e0b7d51f52f1db7b44dfcbb822e03a07d8
Marise St-Pierre <naranjaqc@hotmail.com>
2019-12-01 09:39:23 -0500

    add cardlist to display cards

--- src/Card.js
+++ src/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = ({id, name, email}) => {
     
     return (
-        <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+        <div className='bg-light-green tc dib br3 pa3 ma2 grow bw2 shadow-5'>
             <img src={`https://robohash.org/${id}?200x200`} alt="nice robots"/>
             <div>
     <           h2>{name}</h2>
