6d3cddcf77dd6b87343f591bbfc34d818f8932b3
Marise St-Pierre <naranjaqc@hotmail.com>
2019-12-03 21:21:43 -0500

    Updates

---
+++ precache-manifest.d4a33ed6d3229df460aac8744dc7bfdb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "519ff9a2fe9c580fcf3a084d66f6cd9f",
+    "url": "/robotfriends/index.html"
+  },
+  {
+    "revision": "641d144bde3acab1c6a6",
+    "url": "/robotfriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "b1957d6940afcb03e75c",
+    "url": "/robotfriends/static/css/main.88557dac.chunk.css"
+  },
+  {
+    "revision": "641d144bde3acab1c6a6",
+    "url": "/robotfriends/static/js/2.25ebf1aa.chunk.js"
+  },
+  {
+    "revision": "b1957d6940afcb03e75c",
+    "url": "/robotfriends/static/js/main.82630604.chunk.js"
+  },
+  {
+    "revision": "606e6ab55686db3a4ccd",
+    "url": "/robotfriends/static/js/runtime-main.2942a7ff.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robotfriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
